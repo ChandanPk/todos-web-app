32267043366fa8a69394f27535f1b75ef192f242
ChandanPk <chandanpk9@gmail.com>
2021-09-02 22:28:10 +0530

    add message

--- src/App.js
+++ src/App.js
@@ -4,24 +4,26 @@ import Tasks from "./components/tasks";
 
 function App() {
 
-  const [tasks, setTasks] = useState(["eat", "drink", "sleep", "etc"]);
-  // const [message, set]
+  const [tasks, setTasks] = useState([]);
+  // const [message, setMessage] = useState(null);
 
   const addTask = (newTask)=> {
       setTasks([...tasks, newTask])
   }
 
   const handleDelete = (item)=> {
-    console.log(item);
     let filterTasks = tasks.filter((i)=> {
       return item !== i;
     });
     setTasks(filterTasks);
   }
 
+  console.log(tasks.length)
+
   return (
     <div className="App">
       <h1>To Do's</h1>
+      { tasks.length === 0 && <p>no tasks until now. Lets start adding..</p>}
       <Tasks tasks={tasks} handleDelete={handleDelete} />
       <AddTask addTask= {addTask} />
     </div>
